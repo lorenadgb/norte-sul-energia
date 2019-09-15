b5bfb55e4621236df466c45eb850a4a5855738d0
Lorena Bento <lorena@vyans.com>
2019-09-15 13:25:16 +1000

    Add humanized attrs to calculadora

--- app/models/calculadora.rb
+++ app/models/calculadora.rb
@@ -8,6 +8,17 @@ class Calculadora
   validates_presence_of :rede_eletrica, :estado, :cidade, :local, :ajuste, :media, if: :acesso_a_rede_eletrica?
   validates_presence_of :rede_eletrica, :nome, :telefone, :email, unless: :acesso_a_rede_eletrica?
 
+  HUMANIZED_ATTRIBUTES = {
+      local:  'Tipo do local da instalação',
+      ajuste: 'Ajuste da tarifa',
+      media:  'Energia por mês'
+  }
+
+  def self.human_attribute_name(attr, options = {}) # 'options' wasn't available in Rails 3, and prior versions.
+    HUMANIZED_ATTRIBUTES[attr.to_sym] || super
+  end
+
+
   private
 
   def acesso_a_rede_eletrica?
